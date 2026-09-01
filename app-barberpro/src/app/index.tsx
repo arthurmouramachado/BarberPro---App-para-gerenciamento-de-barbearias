b20b3f4e4076241ee1d62b897b1c306bd6937c8c
Arthur <arthurmouramachado@gmail.com>
2026-09-01 08:52:27 -0300

    fix: concluído arquivo principal do frontend

--- app-barberpro/src/app/index.tsx
+++ app-barberpro/src/app/index.tsx
@@ -1,11 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import LoginScreen from './LoginScreen'
-import SplashScreen from './SplashScreen'
-import { useFonts, Inter_700Bold, Inter_400Regular  } from '@expo-google-fonts/inter';
-import {AuthProvider} from '@/contexts/AuthContext'
-import { ptBR } from '@/utils/localeCalendarConfig';
-import { LocaleConfig } from 'react-native-calendars';
-import _layout from './Barbeiro/_layout';
+import { ptBR } from "@/utils/localeCalendarConfig";
+import {
+  Inter_400Regular,
+  Inter_700Bold,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import React, { useEffect, useState } from "react";
+import { LocaleConfig } from "react-native-calendars";
+
+import SplashScreen from "./SplashScreen";
+import LoginScreen from "./LoginScreen";
+import { AuthProvider } from "@/contexts/AuthContext";
 
 LocaleConfig.locales["pt-br"] = ptBR;
 LocaleConfig.defaultLocale = "pt-br";
@@ -13,34 +17,34 @@ LocaleConfig.defaultLocale = "pt-br";
 let splashJaMostrada = false;
 
 export default function Index() {
-    const [fontsLoaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     Inter_700Bold,
     Inter_400Regular,
   });
 
-  const[showSplash, setShowSplash] = useState(!splashJaMostrada);
+  const [showSplash, setShowSplash] = useState(!splashJaMostrada);
 
-useEffect(() => {
-  // Se a splash já passou da primeira vez, nem precisamos do timer rodando de novo
-  if (splashJaMostrada) return; 
+  useEffect(() => {
+    // Se a splash já passou da primeira vez, nem precisamos do timer rodando de novo
+    if (splashJaMostrada) return;
 
-  const timer = setTimeout(() => {
-    setShowSplash(false);
-    splashJaMostrada = true; //  Salva na memória externa que a Splash já rodou!
-  }, 1500);
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+      splashJaMostrada = true; //  Salva na memória externa que a Splash já rodou
+    }, 1500);
 
-  return () => clearTimeout(timer); 
-}, []);
+    return () => clearTimeout(timer);
+  }, []);
 
   if (!fontsLoaded) {
-    return null; 
+    return null;
   }
 
-  if(showSplash) return <SplashScreen />
+  if (showSplash) return <SplashScreen />;
 
   return (
     <AuthProvider>
       <LoginScreen/>
     </AuthProvider>
   );
-} 
+}
